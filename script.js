3565b8780f76251c6463fb8514ccebc2d9ac10d6
sampathsrigada <sampathsrigada10@gmail.com>
2026-05-18 21:07:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let prices = document.querySelectorAll(".price")
+
 
 const getSum = () => {
 //Add your code here
+	let sum = 0
+	for(let i of prices){
+		sum= sum+Number(i.innerText)
+	}
+	let row = document.createElement("tr")
+	let cell = document.createElement("td")
+	cell.innerText = sum;
+	row.appendChild(cell)
+	document.querySelector("table").appendChild(row)
+	return sum
+	
   
 };
 
